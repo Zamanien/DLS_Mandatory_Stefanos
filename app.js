1312745bfb16b21bdcb1a6afd830677808e4a74a
Stefanos <sagelastos@gmail.com>
2021-09-20 15:00:54 +0200

    added async call example

--- app.js
+++ app.js
@@ -16,13 +16,14 @@ app.get('/newUser', (req, res) => {
     }
 
     const salt = crypto.randomBytes(128).toString('base64');
-    const hash = crypto.pbkdf2Sync(password, salt, 10000, 512, 'sha512');
+    const hash = crypto.pbkdf2Sync(password, salt, 100000, 512, 'sha512');
 
     users[username] = { salt, hash };
 
     res.sendStatus(200);
 });
 
+// this is the synchronous version, this will makse the program slow
 app.get('/auth', (req, res) => {
     let username = req.query.username || '';
     const password = req.query.password || '';
@@ -34,7 +35,7 @@ app.get('/auth', (req, res) => {
     }
 
     const { salt, hash } = users[username];
-    const encryptHash = crypto.pbkdf2Sync(password, salt, 10000, 512, 'sha512');
+    const encryptHash = crypto.pbkdf2Sync(password, salt, 100000, 512, 'sha512');
 
     if (crypto.timingSafeEqual(hash, encryptHash)) {
         res.sendStatus(200);
@@ -42,5 +43,25 @@ app.get('/auth', (req, res) => {
         res.sendStatus(401);
     }
 });
+
+// this is the async version, should be faster
+// app.get('/auth', (req, res) => {
+//     let username = req.query.username || '';
+//     const password = req.query.password || '';
+
+//     username = username.replace(/[!@#$%^&*]/g, '');
+
+//     if (!username || !password || users[username]) {
+//         return res.sendStatus(400);
+//     }
+
+//     crypto.pbkdf2(password, users[username].salt, 100000, 512, 'sha512', (err, hash) => {
+//         if (users[username].hash.toString() === hash.toString()) {
+//             res.sendStatus(200);
+//         } else {
+//             res.sendStatus(401);
+//         }
+//     });
+// });
 
 app.listen(8080);
